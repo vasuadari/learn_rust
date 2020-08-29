a4749cb59706e463b9fe960831ac0504785dd04e
Vasu Adari <vasuakeel@gmail.com>
2020-08-29 21:07:31 +0530

    Add documentation to fibonacci series

--- 03_fibonacci/src/main.rs
+++ 03_fibonacci/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-    println!("No. of fibonacci numberc to print!");
+    println!("No. of fibonacci numbers to print!");
 
     let mut number = String::new();
 
@@ -11,11 +11,17 @@ fn main() {
     let n: i32 = number.trim().parse()
         .expect("invalid number");
 
+    // `a` will be printed
+    // `b` will store next value
+    // `c` will be used to store sum of a & b and moved to b.
+    //     Its a temporary variable.
     let mut a = 0;
     let mut b = 1;
     let mut c = 0;
 
     for _i in c..n {
+        // uncomment to debug
+        // println!("a: {} b: {} c: {}", a, b, c);
         println!("{}", a);
         c = a + b;
         a = b;
