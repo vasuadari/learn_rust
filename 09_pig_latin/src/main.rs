e9b5b085c9f244cb8684521b68198e0894cec42f
Vasu Adari <vasuakeel@gmail.com>
2020-09-19 19:25:06 +0530

    Refactor pig lation program

--- 09_pig_latin/src/main.rs
+++ 09_pig_latin/src/main.rs
@@ -1,35 +1,30 @@
 use std::io;
 
 fn main() {
-  println!("Convert words to pig latin");
+    println!("Convert words to pig latin");
 
-  loop {
-    println!("Please type a word.");
+    loop {
+        println!("Please type a word.");
 
-    let mut word = String::new();
+        let mut word = String::new();
 
-    io::stdin().read_line(&mut word)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut word)
+            .expect("Failed to read line");
 
-    let word = word.trim();
+        let word = word.trim();
 
-    const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+        const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
 
-    let first_letter = &word[0..1];
-    let latter = &word[1..];
-    let ay = "ay";
-    let hay = "hay";
+        let first_letter = &word[0..1];
+        let latter = &word[1..];
+        let ay = "ay";
+        let hay = "hay";
 
-    let mut starts_with_vowel = false;
-    for i in VOWELS.iter() {
-      if word.starts_with(|x| &x == i) {
-        println!("{}-{}", word, hay);
-        starts_with_vowel = true;
-        break;
-      }
-    };
-    if !starts_with_vowel {
-      println!("{}-{}{}", latter, first_letter, ay);
+        let starts_with_vowel = VOWELS.iter().find(|&c| word.starts_with(|x| &x == c));
+        match starts_with_vowel {
+            Some(_char) => println!("{}-{}", word, hay),
+            None => println!("{}-{}{}", latter, first_letter, ay),
+        }
     }
-  }
 }
