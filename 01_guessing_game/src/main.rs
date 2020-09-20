ce36856a3d03acb3a119cfee28de2ed3452af073
Vasu Adari <vasuakeel@gmail.com>
2020-09-21 00:52:06 +0530

    Add validation for guess to be within the range

--- 01_guessing_game/src/main.rs
+++ 01_guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess a number!");
@@ -8,19 +8,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
-
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
         // read_line accepts mutable reference to read/store input
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        if guess < 1 || guess > 100 {
+            println!("Your guess should be between 1 and 100, got {}", guess);
+            continue;
+        }
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
